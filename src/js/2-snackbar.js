6cccbfd68357e9467276e5e3c7af2ad754d574d9
Andrii Kyrch <andrii.kyrch@gmail.com>
2024-10-05 14:59:40 +0200

    Refactoring snackbar

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -2,19 +2,13 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
 const form = document.querySelector('.form');
-const delayInput = form.querySelector('input[name="delay"]');
-const stateInputs = form.querySelectorAll('input[name="state"]');
 
 form.addEventListener('submit', event => {
   event.preventDefault();
-  const delay = Number(delayInput.value);
-  let selectedState;
-  for (const input of stateInputs) {
-    if (input.checked) {
-      selectedState = input.value;
-      break;
-    }
-  }
+  const delay = Number(event.target.delay.value);
+  const selectedState = event.target.elements.state.value;
+  console.log(selectedState);
+
   const promise = createPromise(delay, selectedState);
   promise
     .then(message => {
